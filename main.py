f6f55e7b00621a3b8afc2537c5a4afed7ca2c487
hashemhoseini1313-cmd <hashemhoseini1313@gmail.com>
2026-08-25 07:29:20 +0330

    Update main.py

--- main.py
+++ main.py
@@ -117,20 +117,8 @@ class ScreenRecorderApp(App):
                 activity.startService(service_intent)
 
             self.status_label.text = ftext("سرویس شروع شد")
-
-            # ✅ نمایش Toast برای تأیید شروع سرویس از پایتون
-            Toast = autoclass('android.widget.Toast')
-            Toast.makeText(activity, "Python started service", Toast.LENGTH_LONG).show()
-
         except Exception as e:
             self.status_label.text = ftext(f"خطا در شروع سرویس: {e}")
-
-            # ✅ نمایش Toast خطا
-            try:
-                Toast = autoclass('android.widget.Toast')
-                Toast.makeText(activity, f"Error: {e}", Toast.LENGTH_LONG).show()
-            except:
-                pass
 
     def start_recording(self, instance):
         if platform != "android":
